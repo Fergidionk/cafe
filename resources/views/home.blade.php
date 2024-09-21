4b3f11f1658e9c9eda32399dc2b30cabc89cd9f5
Fergidionk <168946524+Fergidionk@users.noreply.github.com>
2024-09-21 19:50:01 +0700

    commit pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-[#704029]">
+    @include('partials.navbar')
+</body>
+    <div class="container lg:p-5">
+        <div class="cta">
+            <div class="tulisan">
+                <h1 class="text-3xl text-white font-bold">Coffee Teen</h1>
+            </div>
+            <div class="gambar">
+                <img src="https://source.unsplash.com/1600x900/?nature,water"
+            </div>
+        </div>
+    </div>
+</html>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container mx-auto flex items-center justify-between lg:px-8 lg:py-3">
+        <!-- Bagian Logo -->
+        <a class="navbar-brand flex items-center" href="#">
+            <img src="{{ asset('img/icon.png') }}" class="h-12 w-12 mr-3" alt="Logo">
+        </a>
+        
+        <!-- Bagian Menu -->
+        <div class="navbar-nav flex-grow flex justify-center space-x-4">
+            <a href="#beranda" class="nav-link font-medium text-[#FFFCF3]">Beranda</a>
+            <a href="#produk" class="nav-link font-medium text-[#FFFCF3]">Produk</a>
+            <a href="#blog" class="nav-link font-medium text-[#FFFCF3]">Blog</a>
+            <a href="#kontak" class="nav-link font-medium text-[#FFFCF3]">NoTelp</a>
+        </div>
+    </div>
+</nav>
